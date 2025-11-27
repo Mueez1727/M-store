015b316df5911fc1a91c4b1594b453128400ebfb
Mueez1727 <mueeza044@gmail.com>
2025-11-27 17:12:21 +0500

    scroll to top functionality added changes made only in main index.js file and componenets flder created

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ScrollToTop } from "./components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ScrollToTop /> 
     <App />
   </React.StrictMode>
 );
